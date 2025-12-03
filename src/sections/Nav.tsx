8ef973ef33536f9de544f6f08ae56c6f8aa9da38
Batubo Victory <victorybatubo76@gmail.com>
2025-12-03 17:20:04 +0100

    feat: Add logo to navbar

--- src/sections/Nav.tsx
+++ src/sections/Nav.tsx
@@ -1,7 +1,7 @@
 function Nav(){
     return (
         <nav className='bg-sectionBg text-textStrong'>
-            
+            <img src="\Logo\Logo.png" alt="Runwise logo" />
         </nav>
     )
 }
