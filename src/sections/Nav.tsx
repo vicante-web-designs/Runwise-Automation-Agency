bddbe853dbfefbb28148e1e92e61b343e1cb520b
Batubo Victory <victorybatubo76@gmail.com>
2025-12-03 18:21:34 +0100

    feat: Add logo image and text

--- src/sections/Nav.tsx
+++ src/sections/Nav.tsx
@@ -1,9 +1,13 @@
 function Nav(){
     return (
         <nav className='bg-sectionBg text-textStrong'>
-            <div className='flex items-center'>
-                <img src="\Logo\Logo.png" alt="Runwise logo" />
-                <h3 className='font-headerFont'>
+            <div className='flex items-center mx-auto w-fit gap-2 py-4'>
+                <img
+                 src="\Logo\Logo.png"
+                 alt="Runwise logo"
+                 className='w-15'
+                 />
+                <h3 className='tracking-tighter'>
                     RUNWISE
                 </h3>
             </div>
