8a3016f4c1245e3c102484869486f8ae5faa891d
Batubo Victory <victorybatubo76@gmail.com>
2025-12-04 14:34:16 +0100

    refactor: Put logo logic in it's component and import it in the navbar

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,15 @@
 function Logo(){
     return(
-        <>
-        
-        </>
+        <div className='flex items-center mx-auto w-fit gap-2 py-4'>
+            <img
+                src="\Logo\Logo.png"
+                alt="Runwise logo"
+                className='w-15'
+                />
+            <h3 className='tracking-tighter'>
+                RUNWISE
+            </h3>
+        </div>
     )
 }
 
--- src/sections/Nav.tsx
+++ src/sections/Nav.tsx
@@ -1,16 +1,9 @@
+import Logo from '../components/Logo';
+
 function Nav(){
     return (
         <nav className='bg-sectionBg text-textStrong'>
-            <div className='flex items-center mx-auto w-fit gap-2 py-4'>
-                <img
-                 src="\Logo\Logo.png"
-                 alt="Runwise logo"
-                 className='w-15'
-                 />
-                <h3 className='tracking-tighter'>
-                    RUNWISE
-                </h3>
-            </div>
+            <Logo />
         </nav>
     )
 }
