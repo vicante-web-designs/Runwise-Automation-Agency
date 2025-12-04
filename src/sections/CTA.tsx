cc5e85e714a3517baf1ed2853c9a7b49f5d8e596
Batubo Victory <victorybatubo76@gmail.com>
2025-12-04 15:39:03 +0100

    feat" Add CTA content

--- src/sections/CTA.tsx
+++ src/sections/CTA.tsx
@@ -1,8 +1,17 @@
+import Button from '../components/Button';
+
 function CTA(){
     return(
-        <>
-        
-        </>
+        <section className='flex flex-col items-center py-12 gap-[72px]'>
+            <h3>
+                Ready to Automate Your Business?
+            </h3>
+
+            <Button 
+                label='Get started'
+                type='button'
+            />
+        </section>
     )
 }
 
