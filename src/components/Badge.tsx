c8f1ba9cd1aec3ca3987e908f1c4db2c35735900
Batubo Victory <victorybatubo76@gmail.com>
2025-12-04 10:08:38 +0100

    feat: Put label prop in a paragraph tag

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -2,8 +2,10 @@ import type { BadgeProps } from '../../types/Types'
 
 function Badge ({ label }: BadgeProps){
     return(
-        <div className='py-4 px-6 rounded-full w-fit bg-[rgba(0,35,102,0.4)] border border-primaryColor'>
-            {label}
+        <div className='py-2 px-3 rounded-full w-fit bg-[rgba(0,35,102,0.4)] border border-primaryColor'>
+            <p className='text-[16px]'>
+                {label}
+            </p>
         </div>
     )
 }
