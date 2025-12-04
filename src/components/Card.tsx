afd56b1244e13eb654c343c9b2b6da058c5688f8
Batubo Victory <victorybatubo76@gmail.com>
2025-12-04 13:28:34 +0100

    feat: Add a max width for card content

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ function Card({ icon, title, content }: CardProps){
     const IconComponent = icon as React.ElementType;
 
     return (
-        <article className='flex flex-col p-6 gap-6 bg-[rgba(102,123,163,0.2)] rounded-xl w-fit'>
+        <article className='flex flex-col p-6 gap-6 bg-[rgba(102,123,163,0.2)] rounded-xl w-fit mx-auto'>
             <div className='flex flex-col gap-2'>
                 <div className='p-4 w-14 h-14 rounded-full bg-[rgba(0,35,102,0.4)] border border-primaryColor flex items-center'>
                     {
@@ -22,7 +22,7 @@ function Card({ icon, title, content }: CardProps){
                 </h4>
             </div>
 
-            <p className="sub-text">
+            <p className="sub-text max-w-[330px]">
                 {content}
             </p>
         </article>
