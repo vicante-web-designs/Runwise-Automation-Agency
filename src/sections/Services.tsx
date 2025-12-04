771e08d559222dcbe1a7faf8e39ed037f862f532
Batubo Victory <victorybatubo76@gmail.com>
2025-12-04 11:03:38 +0100

    feat: Create an array of service objects and render them

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -1,6 +1,25 @@
+import type { CardProps } from '../../types/Types';
 import Badge from '../components/Badge';
+import Card from '../components/Card';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
+import { AiOutlineRobot } from "react-icons/ai"
 
 function Services(){
+    // List of services
+    const services: CardProps[] = [
+        {
+            icon:faCog,
+            title:'Process Automation',
+            content:'Manual tasks slow teams and cause errors. Runwise automates workflows and approvals, freeing your team to focus on growth.'
+        },
+
+        {
+            icon:AiOutlineRobot,
+            title:'AI Agents',
+            content:'Repetitive tasks waste time. Runwise AI handles routine responses and reminders, letting your team scale efficiently.'
+        },
+    ]
+    
     return(
         <section>
             <Badge 
@@ -9,6 +28,16 @@ function Services(){
             <h2>
                 Our Core Services
             </h2>
+
+            {
+                services.map(service => (
+                    <Card 
+                        icon={service.icon}
+                        title={service.title}
+                        content={service.content}
+                    />
+                ))
+            }
         </section>
     )
 }
