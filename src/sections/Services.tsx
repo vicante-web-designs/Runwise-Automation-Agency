ea34e7020e11322e049a26678a4b7803e93477cf
Batubo Victory <victorybatubo76@gmail.com>
2025-12-04 09:50:53 +0100

    feat: Add badge content

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -3,6 +3,9 @@ import Badge from '../components/Badge';
 function Services(){
     return(
         <section>
+            <Badge 
+                label='What We Offer'
+            />
             <h2>
                 Our Core Services
             </h2>
