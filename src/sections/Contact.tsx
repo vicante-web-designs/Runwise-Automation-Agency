4dfd21307ea3be6b40872a7ed5dae75b533fc731
Batubo Victory <victorybatubo76@gmail.com>
2025-12-05 18:19:31 +0100

    style: Space section properly

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -3,13 +3,13 @@ import Button from '../components/Button';
 
 function Contact(){
     return(
-        <section className='flex flex-col pageSection'>
+        <section className='flex flex-col gap-10 pageSection'>
             <Badge 
                 label='Contact Us'
             />
 
-            <article className='text-center mx-auto flex flex-col'>
-                <h3>
+            <article className='text-center mx-auto flex flex-col gap-4'>
+                <h3 className='leading-10'>
                     Let's Make Your Business<br /> Run Smarter
                 </h3>
 
@@ -18,8 +18,8 @@ function Contact(){
                 </p>
             </article>
 
-            <form action="" method='POST'  className='flex flex-col'>
-                <div className='flex flex-col *:flex *:flex-col gap-8'>
+            <form action="" method='POST'  className='flex flex-col gap-6'>
+                <div className='flex flex-col *:flex *:flex-col gap-4'>
                     <label>
                         <p className='font-bold flex gap-1'>
                             Name
@@ -30,6 +30,7 @@ function Contact(){
                         <input
                         type="text"
                         placeholder="e.g. Victory Automation"
+                        required
                         />
                     </label>
 
@@ -43,6 +44,7 @@ function Contact(){
                         <input
                         type="text"
                         placeholder="e.g. johndoe@yourbusiness.com"
+                        required
                         />
                     </label>
 
@@ -57,6 +59,7 @@ function Contact(){
                         <input
                         type="text"
                         placeholder="e.g. Operations Lead at TechFlow Ltd"
+                        required
                         />
                     </label>
 
@@ -71,6 +74,7 @@ function Contact(){
                         <input
                         type="text"
                         placeholder="e.g. Too many repetitive approvals slowing down my team"
+                        required
                         />
                     </label>
 
