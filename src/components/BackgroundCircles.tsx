4db302fef092c70dc6ebd26f9136bc5354f7e42d
Batubo Victory <victorybatubo76@gmail.com>
2025-12-06 17:28:11 +0100

    feat: Import BackgroundCirclesProp type

--- src/components/BackgroundCircles.tsx
+++ src/components/BackgroundCircles.tsx
@@ -1,4 +1,6 @@
-function BackgroundCircles({ position }){
+import type { BackgroundCirclesProp } from '../../types/Types';
+
+function BackgroundCircles({ position }: BackgroundCirclesProp){
     return(
         <div className={`absolute ${position} w-64 h-64 bg-linear-to-b from-[#001031] to-[#00FFF2] rounded-full blur-[128px] pointer-events-none -z-10`}>
         
