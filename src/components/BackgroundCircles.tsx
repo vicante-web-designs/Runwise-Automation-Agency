ed70b2d81e83d260201cdebfd4a3919412c5f7ec
Batubo Victory <victorybatubo76@gmail.com>
2025-12-07 15:04:12 +0100

    feat: Make the circles look more premium

--- src/components/BackgroundCircles.tsx
+++ src/components/BackgroundCircles.tsx
@@ -2,7 +2,7 @@ import type { BackgroundCirclesProp } from '../../types/Types';
 
 function BackgroundCircles({ position }: BackgroundCirclesProp){
     return(
-        <div className={`fixed ${position} w-48 h-48 bg-linear-to-b from-[#001031] to-[#00fffb] rounded-full blur-[108px] pointer-events-none -z-10`}>
+        <div className={`fixed ${position} w-24 h-24 sm:w-36 sm:h-36 bg-linear-to-b from-[#001031] to-[#00fffb] rounded-full blur-2xl sm:blur-3xl pointer-events-none -z-10`}>
         
         </div>
     )
